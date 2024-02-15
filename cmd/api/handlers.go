d75ddda5f9fcd90b789b7c9d4e1fa44c1a3c89ed
Brian Green <bgreen007@cox.net>
2024-02-15 14:38:14 -0600

    linter fixes

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -16,19 +16,19 @@ func (a *App) Authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := helpers.ReadJSON(w, r, &requestPayload); err != nil {
-		helpers.ErrorJSON(w, err, http.StatusBadRequest)
+		_ = helpers.ErrorJSON(w, err, http.StatusBadRequest)
 		return
 	}
 
 	user, err := a.UserRepo.GetByEmail(requestPayload.Email)
 	if err != nil {
-		helpers.ErrorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
+		_ = helpers.ErrorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
 	valid, err := user.PasswordMatches(requestPayload.Password)
 	if err != nil || !valid {
-		helpers.ErrorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
+		_ = helpers.ErrorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
@@ -36,5 +36,5 @@ func (a *App) Authenticate(w http.ResponseWriter, r *http.Request) {
 		Error:   false,
 		Message: fmt.Sprintf("Logged in user %s", user.Email),
 	}
-	helpers.WriteJSON(w, http.StatusAccepted, payload)
+	_ = helpers.WriteJSON(w, http.StatusAccepted, payload)
 }
